bc29a0f117f8d89f73908a679a37774280906920
hamedkhodami <hamedkhodami977@gmail.com>
2025-07-09 16:00:36 +0330

    admin account

--- src/apps/account/admin.py
+++ src/apps/account/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from . import models
+
+
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+
+    model = models.User
+
+    list_display = ('phone_number', 'email', 'is_active', 'last_login', 'role')
+    list_filter = ('is_active', 'role', 'first_name', 'last_name')
+    fieldsets = (
+        (None, {'fields': (
+            'phone_number', 'email', 'password', 'role', 'national_id', 'first_name',
+            'last_name',)}),
+        ('Permissions',
+         {'fields': ('is_active', 'is_phone_number_confirmed', 'is_national_id_confirmed')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('phone_number', 'email', 'password1', 'password2', 'is_active',
+                       'is_phone_number_confirmed', 'first_name', 'last_name', 'national_id',
+                       'role')}
+         ),
+    )
+    search_fields = ('phone_number',)
+    ordering = ('phone_number',)
+    filter_horizontal = ()
+
+
+admin.site.register(models.User, CustomUserAdmin)
+admin.site.register(models.UserBlock)
+admin.site.register(models.UserProfileModel)
